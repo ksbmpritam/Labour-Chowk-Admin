40976446d7e1c945fe1022fdd30717701fcd2d1d
gaurav-gunjan <ksbmksbm34@gmail.com>
2024-04-03 18:42:23 +0530

    Sub Skill Api Route Added

--- src/utils/apiRoutes/index.js
+++ src/utils/apiRoutes/index.js
@@ -5,4 +5,7 @@ export const update_skill = "api/admin/update_skill_byID";
 export const delete_skill = "api/admin/delete_skill_byID";
 
 //! Sub Skill
+export const get_sub_skill = "api/admin/get_all_subSkill";
 export const create_sub_skill = "api/admin/add_subSkill";
+export const update_sub_skill = "api/admin/update_subskill_byID";
+export const delete_sub_skill = "api/admin/delete_subskill_byID";
