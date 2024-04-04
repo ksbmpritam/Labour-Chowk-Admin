7e62ac9ed9651c8804e310471c7a13b0178768bf
gaurav-gunjan <ksbmksbm34@gmail.com>
2024-04-04 17:47:02 +0530

    Sweet-Alert Added in Change Status API

--- src/redux/saga/partnerSaga.js
+++ src/redux/saga/partnerSaga.js
@@ -71,16 +71,84 @@ function* changePartnerStatus(action) {
         console.log("Payload ::: ", payload)
         console.log("Payload ID ::: ", payload?.labourID)
 
-        const { data } = yield call(axios.post, `${api_urls + change_partner_status}`, payload);
-        console.log("Change Partner Status Saga Response ::: ", data)
+        const result = yield Swal.fire({
+            title: `Are You Sure To Change Status`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#2A9BAA",
+            cancelButtonColor: "red",
+            confirmButtonText: "Yes",
+            cancelButtonText: "No"
+        })
 
-        if (data?.success) {
-            yield put({ type: actionTypes.GET_PARTNER_BY_ID, payload: { labourID: payload?.labourID } })
-            yield put({ type: actionTypes.GET_ALL_PARTNER, payload: null })
+        if (result.isConfirmed) {
+            const { data } = yield call(axios.post, `${api_urls + change_partner_status}`, payload);
+            console.log("Change Partner Status Saga Response ::: ", data)
+
+            if (data.success) {
+                Swal.fire({
+                    icon: "success",
+                    title: "Status Change Successfully",
+                    showConfirmButton: false,
+                    timer: 2000,
+                });
+                yield put({ type: actionTypes.GET_PARTNER_BY_ID, payload: { labourID: payload?.labourID } })
+                yield put({ type: actionTypes.GET_ALL_PARTNER, payload: null })
+            }
         }
 
     } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Server Error",
+            text: "Failed To Change Status",
+            showConfirmButton: false,
+            timer: 2000,
+        });
         console.log("Change Partner Status Saga Error ::: ", error)
+    }
+}
+
+function* deleteSubSkill(action) {
+    try {
+        const { payload } = action;
+        console.log("Payload ::: ", payload)
+
+        const result = yield Swal.fire({
+            title: `Are You Sure To Delete`,
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#2A9BAA",
+            cancelButtonColor: "red",
+            confirmButtonText: "Delete",
+        })
+
+        console.log("Result ::: ", result)
+
+        if (result.isConfirmed) {
+            const { data } = yield call(axios.post, `${api_urls + delete_sub_skill}`, payload);
+            console.log("Delete Sub Skill Saga Response ::: ", data)
+            if (data.success) {
+                Swal.fire({
+                    icon: "success",
+                    title: "Sub Skill Deleted Successfully",
+                    showConfirmButton: false,
+                    timer: 2000,
+                });
+                yield put({ type: actionTypes.GET_SUB_SKILL, payload: null })
+
+            }
+        }
+    } catch (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Server Error",
+            text: "Failed To Delete Sub Skill",
+            showConfirmButton: false,
+            timer: 2000,
+        });
+        console.log("Delete Sub Skill Saga Error ::: ", error)
     }
 }
 
