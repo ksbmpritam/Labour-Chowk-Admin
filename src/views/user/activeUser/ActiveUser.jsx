41ab8365e91e30bef318b36cef4333f810f55b28
gaurav-gunjan <ksbmksbm34@gmail.com>
2024-04-08 11:11:41 +0530

    Active  | Banned Data Get

--- src/views/user/activeUser/ActiveUser.jsx
+++ src/views/user/activeUser/ActiveUser.jsx
@@ -1,48 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import CIcon from '@coreui/icons-react';
 import DataTable from 'react-data-table-component';
 import * as icon from '@coreui/icons';
 import { DataTableCustomStyles } from '../../../styles';
 import { useNavigate } from 'react-router-dom';
 import DataTableHeader from '../../../components/common/DataTableHeader';
-import pictureIcon from "../../../assets/images/avatars/4.jpg";
+import * as UserActions from '../../../redux/actions/userAction';
 
 const ActiveUser = () => {
-  const navigate = useNavigate();
-
-    //! User Start
-    const userData = [
-        {
-            id: 1,
-            name: "User One",
-            email: "userone@gmail.com",
-            contact: "8757858745",
-            profileImage: pictureIcon,
-            aadhar: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/Aadhaar_Logo.svg/800px-Aadhaar_Logo.svg.png",
-        },
-        {
-            id: 2,
-            name: "User Two",
-            email: "usertwo@gmail.com",
-            contact: "8709858745",
-            profileImage: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_640.jpg",
-            aadhar: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/Aadhaar_Logo.svg/800px-Aadhaar_Logo.svg.png",
-        }
-    ]
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { activeUserData: userData } = useSelector((state) => state?.userReducer);
+    console.log("Active User Data :: ", userData);
 
     const userColumns = [
-        {
-            name: 'S.No',
-            selector: (row, index) => index + 1,
-        },
-        {
-            name: 'Name',
-            selector: row => row.name,
-        },
-        {
-            name: 'Email',
-            selector: row => row.email,
-        },
+        { name: 'S.No.', selector: row => userData.indexOf(row) + 1, style: { backGroundColor: "#000", paddingLeft: "20px" } },
+        { name: 'Name', selector: row => row.userName },
+        { name: 'Email', selector: row => row.email },
         {
             name: 'Profile Image',
             cell: row => <img src={row.profileImage} alt="Profile" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />,
@@ -51,34 +26,39 @@ const ActiveUser = () => {
             name: 'Aadhar',
             cell: row => <img src={row.aadhar} alt="Profile" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />,
         },
-        {
-            name: 'Mobile',
-            selector: row => row.contact,
-        },
+        { name: 'Mobile', selector: row => row.phoneNo },
         {
             name: 'Action',
             cell: row => <div style={{ display: "flex", gap: "20px", alignItems: "center" }} >
-                <CIcon data-tooltip-id="my-tooltip" data-tooltip-content="View" style={{ cursor: "pointer" }} onClick={() => handleView(row)} icon={icon.cilTouchApp} size="lg" />
+                <CIcon data-tooltip-id="my-tooltip" data-tooltip-content="View" style={{ cursor: "pointer" }} onClick={() => navigate(`/user/${row?._id}`)} icon={icon.cilTouchApp} size="lg" />
             </div>,
         },
     ]
-    //! User End
-    const handleView = (data) => {
-        console.log("View Data ::: ", data)
-        navigate(`/user/${data?.id}`);
-    }
 
-  return (
-    <div style={{ padding: "20px", backgroundColor: "#fff" }}>
-                <DataTableHeader title={'Active Users'} data={userData} />
-                <DataTable
-                    columns={userColumns}
-                    data={userData}
-                    pagination
-                    customStyles={DataTableCustomStyles}
-                />
-    </div>
-  )
+    useEffect(function () {
+        //! Dispatching API for Getting Active user
+        dispatch(UserActions.getActiveUser())
+    }, []);
+
+    return (
+        <>
+            {
+                userData &&
+                <div style={{ padding: "20px", backgroundColor: "#fff" }}>
+                    <DataTableHeader title={'Active Users'} data={userData} />
+                    <DataTable
+                        columns={userColumns}
+                        data={userData}
+                        pagination
+                        customStyles={DataTableCustomStyles}
+                        paginationPerPage={5}
+                        paginationRowsPerPageOptions={[5, 10, 15, 20]}
+                        paginationComponentOptions={{ rowsPerPageText: 'Rows Per Page :' }}
+                    />
+                </div>
+            }
+        </>
+    )
 }
 
 export default ActiveUser
--- src/views/user/bannedUser/BannedUser.jsx
+++ src/views/user/bannedUser/BannedUser.jsx
@@ -1,87 +1,79 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import CIcon from '@coreui/icons-react';
 import DataTable from 'react-data-table-component';
 import * as icon from '@coreui/icons';
 import { DataTableCustomStyles } from '../../../styles';
 import { useNavigate } from 'react-router-dom';
 import DataTableHeader from '../../../components/common/DataTableHeader';
-import pictureIcon from "../../../assets/images/avatars/4.jpg";
+import * as UserActions from '../../../redux/actions/userAction';
 
 const BannedUser = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { bannedUserData: userData } = useSelector((state) => state?.userReducer);
+    console.log("Banned User Data :: ", userData);
 
-  const navigate = useNavigate();
+    const userColumns = [
+        {
+            name: 'S.No',
+            selector: (row, index) => index + 1,
+        },
+        {
+            name: 'Name',
+            selector: row => row.name,
+        },
+        {
+            name: 'Email',
+            selector: row => row.email,
+        },
+        {
+            name: 'Profile Image',
+            cell: row => <img src={row.profileImage} alt="Profile" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />,
+        },
+        {
+            name: 'Aadhar',
+            cell: row => <img src={row.aadhar} alt="Profile" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />,
+        },
+        {
+            name: 'Mobile',
+            selector: row => row.contact,
+        },
+        {
+            name: 'Action',
+            cell: row => <div style={{ display: "flex", gap: "20px", alignItems: "center" }} >
+                <CIcon data-tooltip-id="my-tooltip" data-tooltip-content="View" style={{ cursor: "pointer" }} onClick={() => handleView(row)} icon={icon.cilTouchApp} size="lg" />
+            </div>,
+        },
+    ]
 
-  //! User Start
-  const userData = [
-      {
-          id: 1,
-          name: "User One",
-          email: "userone@gmail.com",
-          contact: "8757858745",
-          profileImage: pictureIcon,
-          aadhar: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/Aadhaar_Logo.svg/800px-Aadhaar_Logo.svg.png",
-      },
-      {
-          id: 2,
-          name: "User Two",
-          email: "usertwo@gmail.com",
-          contact: "8709858745",
-          profileImage: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_640.jpg",
-          aadhar: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/Aadhaar_Logo.svg/800px-Aadhaar_Logo.svg.png",
-      }
-  ]
+    //! User End
+    const handleView = (data) => {
+        console.log("View Data ::: ", data)
+        navigate(`/user/${data?.id}`);
+    }
 
-  const userColumns = [
-      {
-          name: 'S.No',
-          selector: (row, index) => index + 1,
-      },
-      {
-          name: 'Name',
-          selector: row => row.name,
-      },
-      {
-          name: 'Email',
-          selector: row => row.email,
-      },
-      {
-          name: 'Profile Image',
-          cell: row => <img src={row.profileImage} alt="Profile" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />,
-      },
-      {
-          name: 'Aadhar',
-          cell: row => <img src={row.aadhar} alt="Profile" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />,
-      },
-      {
-          name: 'Mobile',
-          selector: row => row.contact,
-      },
-      {
-          name: 'Action',
-          cell: row => <div style={{ display: "flex", gap: "20px", alignItems: "center" }} >
-              <CIcon data-tooltip-id="my-tooltip" data-tooltip-content="View" style={{ cursor: "pointer" }} onClick={() => handleView(row)} icon={icon.cilTouchApp} size="lg"/>
-          </div>,
-      },
-  ]
-  //! User End
-  const handleView = (data) => {
-      console.log("View Data ::: ", data)
-      navigate(`/user/${data?.id}`);
-  }
+    useEffect(function () {
+        //! Dispatching API for Getting Banned user
+        dispatch(UserActions.getBannedUser())
+    }, []);
 
-  return (
-    <>
-    <div style={{ padding: "20px", backgroundColor: "#fff" }}>
-        <DataTableHeader title={'Banned User'} data={userData} />
-        <DataTable
-            columns={userColumns}
-            data={userData}
-            pagination
-            customStyles={DataTableCustomStyles}
-        />
-    </div>
-</>
-  )
+    return (
+        <>
+            {
+                userData &&
+                <div style={{ padding: "20px", backgroundColor: "#fff" }}>
+                    <DataTableHeader title={'Banned User'} data={userData} />
+                    <DataTable
+                        columns={userColumns}
+                        data={userData}
+                        pagination
+                        customStyles={DataTableCustomStyles}
+                    />
+                </div>
+            }
+        </>
+    )
 }
 
 export default BannedUser
