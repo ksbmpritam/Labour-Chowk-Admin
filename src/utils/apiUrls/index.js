0d323a119b255b3239929c5bec14cff910f9947c
gaurav-gunjan <ksbmksbm34@gmail.com>
2024-04-11 09:45:24 +0530

    API URL

--- src/utils/apiUrls/index.js
+++ src/utils/apiUrls/index.js
@@ -1,8 +1,8 @@
 export let api_urls;
 
 if (window.location.protocol === 'http:') {
-    // api_urls = 'http://13.232.218.5:5000/'
-    api_urls = 'http://192.168.29.99:5000/'
+    api_urls = 'http://13.232.218.5:5000/'
+    // api_urls = 'http://192.168.29.99:5000/'
 } else if (window.location.protocol === 'https:') {
     api_urls = 'http://13.232.218.5:5000/'
     console.log('Running HTTPS', api_urls);
